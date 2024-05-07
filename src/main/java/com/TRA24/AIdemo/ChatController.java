2a683dc79573a4ae2f30551ab901b1818b4ffdbb
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-05-07 19:10:36 +0400

    created the function in ChatController

--- src/main/java/com/TRA24/AIdemo/ChatController.java
+++ src/main/java/com/TRA24/AIdemo/ChatController.java
@@ -1,4 +1,21 @@
 package com.TRA24.AIdemo;
 
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @GetMapping("chat")
+    public String generate(@RequestParam(value = "message", defaultValue = "What is AI Chatbot?") String message){
+        return chatClient.call(message);
+    }
 }
